b2248b1c0bc42ef9ac2ec2354c062e0abef880ce
clifford-indee <clifford@indee.tv>
2025-09-29 12:48:58 +0530

    Update env variable

--- tests/stepdefinitions/test_adminLogin.py
+++ tests/stepdefinitions/test_adminLogin.py
@@ -2,8 +2,6 @@ import os
 import pytest
 
 from dotenv import load_dotenv
-from pathlib import Path
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,7 +12,7 @@ from selenium.common.exceptions import TimeoutException
 
 # dotenv_path = Path('D:\Clifford Personal\Projects\learn-indee\.env')
 load_dotenv()
-url = os.getenv('URL')
+url = os.getenv('ADMIN_URL')
 acc_name = os.getenv('ACC_THE')
 acc_key = os.getenv('KEY_THE')
 
