322128abd72783ab6f737cf344b0aafd72a2e7be
clifford-indee <clifford@indee.tv>
2025-09-29 11:14:00 +0530

    Environment var

---
+++ tests/stepdefinitions/test_adminLogin.py
@@ -0,0 +1,20 @@
+import os
+import pytest
+
+from dotenv import load_dotenv
+from pathlib import Path
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+
+# dotenv_path = Path('D:\Clifford Personal\Projects\learn-indee\.env')
+load_dotenv()
+url = os.getenv('URL')
+acc_name = os.getenv('ACC_THE')
+acc_key = os.getenv('KEY_THE')
+
