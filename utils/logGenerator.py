8ddfbe85b56b65a6a609f2dfa49ad7900512c9d0
clifford-indee <clifford@indee.tv>
2025-10-07 14:39:55 +0530

    Fix code errors in logGenerator file.

--- utils/logGenerator.py
+++ utils/logGenerator.py
@@ -3,15 +3,15 @@ import time
 
 class Logger():
     def __init__(self, logger, file_level=logging.INFO):
-        self.logger = logger.getLogger(logger)
+        self.logger = logging.getLogger(logger)
         self.logger.setLevel(logging.DEBUG)
 
         # formatter
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        cur_time = time.struct_time("%y-%m-%d")
+        cur_time = time.strftime("%Y-%m-%d", time.localtime())
 
         # log file
-        self.LogFileName = '..\\logs\\log'+cur_time+'.txt'
+        self.LogFileName  = ".\\logs\\log"+cur_time+".txt"
         file_handler = logging.FileHandler(self.LogFileName, mode='a')
         file_handler.setFormatter(formatter)
         file_handler.setLevel(file_level)
